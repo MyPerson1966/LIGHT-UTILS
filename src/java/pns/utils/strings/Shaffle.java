ebd42267c4663465ff0a01fedecc85cbf671e6a4
PSEVO tochka <PSEVO tochka@polutoshiba1>
2017-10-30 14:57:04 +0300

    30-10-17 UserCreation Add Types ant preparing for adding user's Telescopes Conversation Dialog Of CreateUser   Coloring REMAKE user API

--- src/java/pns/utils/strings/Shaffle.java
+++ src/java/pns/utils/strings/Shaffle.java
@@ -13,18 +13,17 @@ import java.util.List;
  * @author jpdw
  */
 class Shaffle {
-    
-    
-    public String shuffle(String input){
-        List<Character> characters = new ArrayList<Character>();
-        for(char c:input.toCharArray()){
-            characters.add(c);
-        }
-        StringBuilder output = new StringBuilder(input.length());
-        while(characters.size()!=0){
-            int randPicker = (int)(Math.random()*characters.size());
-            output.append(characters.remove(randPicker));
-        }
-        return output.toString();
+
+    public String shuffle(String input) {
+	List<Character> characters = new ArrayList<Character>();
+	for (char c : input.toCharArray()) {
+	    characters.add(c);
+	}
+	StringBuilder output = new StringBuilder(input.length());
+	while (characters.size() != 0) {
+	    int randPicker = (int) (Math.random() * characters.size());
+	    output.append(characters.remove(randPicker));
+	}
+	return output.toString();
     }
 }
